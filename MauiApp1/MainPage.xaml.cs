2fbaf3925bd1e4b1e5b6cea6a0627e20e8f38e68
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2023-11-06 20:07:44 +0530

    initial commit.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+using Syncfusion.Maui.DataGrid;
+using Syncfusion.Maui.Inputs;
+
+namespace MauiApp1
+{
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
+
